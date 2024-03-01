cb38f506bc5eedd8f4d7852bb7022237edd79eba
Santyaga7 <149270626+Santyaga7@users.noreply.github.com>
2024-03-01 15:09:56 +0200

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,48 +1,31 @@
-const getTotalBalanceByGender=(users, gender)=>{
-    const totalBalanceByGender = users 
-    .filter(user => user.gender === gender)
-    .reduce ((total, user) => {return total+user.balance}, 0);
-    return totalBalanceByGender;
-}
 
-const clients = [
-	{
-    name: "Moore Hensley",
-    gender: "male",
-    balance: 2811
-  },
-  {
-    name: "Sharlene Bush",
-    gender: "female",
-    balance: 3821
-  },
-  {
-    name: "Ross Vazquez",
-    gender: "male",
-    balance: 3793
-  },
-  {
-    name: "Elma Head",
-    gender: "female",
-    balance: 2278
-  },
-  {
-    name: "Carey Barr",
-    gender: "male",
-    balance: 3951
-  },
-  {
-    name: "Blackburn Dotson",
-    gender: "male",
-    balance: 1498
-  },
-  {
-    name: "Sheree Anthony",
-    gender: "female",
-    balance: 2764
+class Storage {
+  #items
+    constructor({ items }) {
+      this.#items = items;
+    }
+    getItems() {
+      return this.#items;
+    }
+    addItem(newItem) {
+      this.#items.push(newItem);
+      return this.#items
+    }
+    removeItem(itemToRemove) {
+        const index = this.#items.indexOf(itemToRemove);
+        if (index !== -1) {
+          this.#items.splice(index, 1);
+          return true; 
+        }
+        return false; 
+      }
   }
-];
+  
+  const storage = new Storage({ items: ["Nanitoids", "Prolonger", "Antigravitator"] });
 
-console.log(getTotalBalanceByGender(clients, "male")); // 12053
-
-console.log(getTotalBalanceByGender(clients, "female")); // 8863
+  console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+  storage.addItem("Droid");
+  console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+  storage.removeItem("Prolonger");
+  console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+  
