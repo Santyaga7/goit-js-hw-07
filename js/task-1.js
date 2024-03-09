c7ef74cdd0e08610341ed8ab170c617635bcf4dd
Santyaga7 <149270626+Santyaga7@users.noreply.github.com>
2024-03-09 18:17:02 +0200

    09.03.24
    09.03.24

--- js/task-1.js
+++ js/task-1.js
@@ -1,31 +1,16 @@
-const customer = {
-    username: "Mango",
-    balance: 24000,
-    discount: 0.1,
-    orders: ["Burger", "Pizza", "Salad"],
-    // Change code below this line
-    getBalance() {
-      return this.balance;
-    },
-    getDiscount() {
-      return this.discount;
-    },
-    setDiscount(value) {
-      this.discount = value;
-    },
-    getOrders() {
-      return this.orders;
-    },
-    addOrder(cost, order) {
-      this.balance -= cost - cost * this.discount;
-      this.orders.push(order);
-    },
-    // Change code above this line
-  };
-  
-  customer.setDiscount(0.15);
-  console.log(customer.getDiscount()); // 0.15
-  customer.addOrder(5000, "Steak");
-  console.log(customer.getBalance()); // 19750
-  console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
-  
+const categories = document.querySelector('#categories');
+const categoriesCount = categories.querySelectorAll('li.item').length;
+console.log(`Number of categories: ${categoriesCount}`);
+
+const listItems = categories.querySelectorAll('h2')
+
+listItems.forEach (function (currentListItem) {
+    const subCategoriesCount = currentListItem.nextElementSibling.querySelectorAll('li').length;
+    console.log (`Category: ${currentListItem.textContent}`);
+    console.log (`Elements: ${subCategoriesCount}`);
+})
+
+
+
+   
+    
--- js/task-3.js
+++ js/task-3.js
@@ -1,35 +1,11 @@
-class StringBuilder {
-    #value;
+const textInput = document.querySelector ("#name-input");
 
-    constructor({ initialValue }) {
-        this.#value = initialValue; 
+const output = document.querySelector ("#name-output");
+textInput.addEventListener("input", () => {
+    const trimmedValue = textInput.value.trim();
+    if (trimmedValue === "") {
+        output.textContent = "Anonymous";
+    } else {
+        output.textContent = trimmedValue;
     }
-
-    getValue() {
-        return this.#value;
-    }
-
-    padEnd(str) {
-        this.#value += str; 
-        return this.#value;
-    }
-
-    padStart(str) {
-        this.#value = str + this.#value; 
-        return this.#value;
-    }
-
-    padBoth(str) {
-        this.#value = str + this.#value + str; 
-        return this.#value;
-    }
-}
-
-const builder = new StringBuilder({ initialValue: "." });
-console.log(builder.getValue()); // "."
-builder.padStart("^");
-console.log(builder.getValue()); // "^."
-builder.padEnd("^");
-console.log(builder.getValue()); // "^.^"
-builder.padBoth("=");
-console.log(builder.getValue()); // "=^.^="
+});
